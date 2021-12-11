206f6758e3a372ec1b9c01e64bd77ccaa8f9fc1b
WOOSHIK-M <wooshik.m@gamil.com>
2021-12-12 00:00:57 +0900

    Print abstract of paper

--- src/main.py
+++ src/main.py
@@ -7,9 +7,22 @@ Author:
 """
 
 import ssl
+from typing import Dict
 from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
+
+
+def paper_summary(paper_info: Dict[str, str]) -> None:
+    """Pretty print paper summary."""
+    str_length = len(paper_info["title"]) + 10
+    print("=" * str_length)
+    print(f"{paper_info['title']}".center(str_length))
+    print("=" * str_length)
+    print(f"-> (ABSTRACT): {paper_info['abstract']}")
+    print(f"-> (LINK): {paper_info['href']}")
+    print()
+
 
 keywords = input("keywords: ").split(",")
 assert keywords, "Plaese give me more than a keyword(s)!!!"
@@ -20,30 +33,27 @@ for i, keyword in enumerate(keywords):
     print(f"keyword_{i}: {keyword}")
 
 # convert keywords to url
-print("\n=> URL Generating ... ")
 GOOGLE_SCHOLAR_PRE = "https://scholar.google.com/scholar?hl=ko&as_sdt=0%2C5&q="
 GOOGLE_SCHOLAR_POST = "&btnG="
 GOOGLE_SCHOLAR_KEYS = "%2C+".join(keywords)
 URL = GOOGLE_SCHOLAR_PRE + GOOGLE_SCHOLAR_KEYS + GOOGLE_SCHOLAR_POST
+print("URL:", URL)
 
-# https forbidden
+# https access
 print("\n=> Crawling ...")
 context = ssl._create_unverified_context()  # pylint: disable=W0212
-
-URL = "https://scholar.google.com/scholar?hl=ko&as_sdt=0%2C5&q=hih&btnG="
 request_url = Request(URL, headers={"User-Agent": "Mozilla/5.0"})
 html = urlopen(request_url, context=context).read()
-
 soup = BeautifulSoup(html, "html.parser")
 
-blocks = soup.find_all(class_="gs_rt")
-
+blocks = soup.find_all(class_="gs_ri")
 for block in blocks:
-    elements = block.find("a")
+    title_block = block.find(class_="gs_rt")
 
-    title = elements.text
-    href = elements.attrs["href"]
-
-    print(title)
-    print(href)
-    print()
+    paper_info: Dict[str, str] = {
+        "title": title_block.text,
+        "href": title_block.find("a").attrs["href"],
+        "journal_name": block.find(class_="gs_a").text,
+        "abstract": block.find(class_="gs_rs").text,
+    }
+    paper_summary(paper_info)
